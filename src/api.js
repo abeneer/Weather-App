5533fa360319e79ae80b94df49ec88de9218d5ec
Abeneer <absaipogu@gmail.com>
2023-06-12 21:54:29 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -2,16 +2,16 @@ export const geoApiOptions = {
     method: 'GET',
     headers: {
     
-        'X-RapidAPI-Key': 'd7138c68b7msha29e17fca444e15p13d5f3jsnde27f4307518',
+        // 'X-RapidAPI-Key': 'write rapidAPIkey',
 
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        // 'X-RapidAPI-Host': 'write X-RapidAPI-Host'
 
     }
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+// export const GEO_API_URL =   "write here GEO_API_URL";
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+// export const WEATHER_API_URL = "write here open weather API URl"
 
-export const WEATHER_API_KEY = "a2b1874fcd826df9bc0da89ef1ed95a9"
+// export const WEATHER_API_KEY = "write here weather api key"
 
